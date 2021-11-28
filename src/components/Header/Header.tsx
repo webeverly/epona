2cc5c63241ec2964cf34e0217e4ebdb29e249ac7
Beverly Cagelet <beverly.cagelet@akinox.com>
2021-11-28 20:57:33 +0100

    theseee changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "../../styles/Home.module.css";
+import styles from "./Header.module.scss";
 import { SearchByImageModal } from "../SearchByImageModal";
 
 interface HeaderProps {
