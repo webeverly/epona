c5625df835f70e345ca7ee733877402dbe8d312d
Christian Droulers <droulers.christian@gmail.com>
2021-11-28 12:03:08 -0500

    moar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { loadData } from "../data/produce";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+loadData();
