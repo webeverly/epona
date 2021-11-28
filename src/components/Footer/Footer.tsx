e0f1564ba1728d27e825e380410071d6e2bf9a1b
Christian Droulers <droulers.christian@gmail.com>
2021-11-28 16:49:45 -0500

    Smol CSS fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,5 @@
 import styles from "./Footer.module.scss";
 
 export const Footer = (): JSX.Element => {
-  return (
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Climactériquemoélsac
-      </a>
-    </footer>
-  );
+  return <footer className={styles.footer}>Climacteric</footer>;
 };
