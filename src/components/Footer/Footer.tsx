9f9550a35720017ee00fd5c678d48f2b017c72a0
Christian Droulers <droulers.christian@gmail.com>
2021-11-28 16:55:34 -0500

    Bring back footer, fix style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import styles from "./Footer.module.scss";
 
 export const Footer = (): JSX.Element => {
-  return <footer className={styles.footer}>Climacteric</footer>;
+  return <footer className={styles.footer}>© Climacteric</footer>;
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Footer } from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="root">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
 export default MyApp;
