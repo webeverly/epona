215f71c0c5df8109e210aa23aebf9bc84c77ddfd
Christian Droulers <droulers.christian@gmail.com>
2021-11-28 10:54:23 -0500

    eslint?

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const settings = {
   reactStrictMode: true,
-}
+};
+
+module.exports =
+  process.env.NODE_ENV === "development" ? settings : withPWA(settings);
