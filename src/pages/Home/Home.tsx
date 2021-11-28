fd87654f6770b5eb2c96854206dc884062fbf009
Beverly Cagelet <beverly.cagelet@akinox.com>
2021-11-28 18:11:00 +0100

    New changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
+import styles from "./Home.module.scss";
 import { useState } from "react";
 import { Footer } from "../../Footer";
 import { Header } from "../../Header";
